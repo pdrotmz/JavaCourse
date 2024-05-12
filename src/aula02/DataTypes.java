dbdd2997bec1fe0d141e54c173ea7b2de74bdbf3
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-12 17:57:11 -0300

    feat(aula02): DataTypes in java

---
+++ src/aula02/DataTypes.java
@@ -0,0 +1,65 @@
+package aula02;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        /**  Data Types
+         *    
+         *    - Integer -> byte, short, long, int
+         *    - Float   -> double, float
+         *    - Character -> char, String
+         *    - Boolean 
+         * 
+         */
+
+         /** Integer
+          * 
+          *   - byte -> 1 byte
+          *   - short -> 2 bytes
+          *   - int -> 4 bytes
+          *   - long -> 8 bytes
+          *   
+          */
+        
+         /** Float
+          * 
+          *   - float -> 4 bytes
+          *   - double -> 8 bytes
+          *
+          *  Float VS Double
+          *    Double has more precision when
+          *    you working with longer numbers,
+          *    and turns the execution faster than
+          *    when you working with float numbers
+          *
+          */
+
+
+         /**  Character
+          *   
+          *    - char -> 2 bytes / UNICODE
+          * 
+          */
+          
+
+         /**  Booolean
+          * 
+          *    - True
+          *    - False
+          * 
+          */
+
+          int firstNUmber = 9;
+          byte by = 127;
+          short sh = 558;
+          long lon = 5854l;
+
+          float f = 5.8f;
+          double d = 5.8;
+
+          char c = 'k';
+
+          boolean b = true;
+
+    }
+}
