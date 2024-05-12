0c54b9a7734e00f2569370e130914f4c16511dc9
pdrotmz <pedrotomaz0210@gmail.com>
2024-05-12 17:45:34 -0300

    intial commit

---
+++ src/aula01/Variables.java
@@ -0,0 +1,26 @@
+package aula01;
+
+public class Variables {
+    public static void main(String[] args) {
+        /**
+         * Variables are storages for datas,
+         * imagine that, you have the number 23,
+         * and you want to storage it, just
+         * create a box called number, just 
+         * like this:
+         */
+
+         int firstNumber = 23;
+         int secondNumber = 23;
+         int result = firstNumber + secondNumber;
+
+         /**
+          * Basically 23 is a number, in Java
+          * when we have a integer number, we
+          * we called it an int(from integer)
+          * this gonna be your variable type
+          */
+
+          System.out.println(result);
+    }
+}
